16e607e8a44640c0097bd46069bc2442e08b4e68
CheeezBurger <140988965+CheeezBurger@users.noreply.github.com>
2024-08-30 14:04:23 +0800

    Create Basics.cpp

---
+++ Basics.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+  std::cout << "Hello World!" << '\n';
+  std::cout <<"Helooo Woorrllldddd" <<std::endl;
+  return 0;
+}
+
+  //This is a comment. To make one, just use two //.
+  
+  /*
+  This is a
+  multi-line comment.
+  It lasts forever.
+
+  Comments are ignored by the computer
+  */
