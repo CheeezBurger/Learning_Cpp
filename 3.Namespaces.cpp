5af77920c41ff630324f6572bf94f77d0c545993
CheeezBurger <140988965+CheeezBurger@users.noreply.github.com>
2024-08-30 14:38:03 +0800

    Create 3.Namespaces.cpp

---
+++ 3.Namespaces.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+/* Namespaces provide a solution for preventing name conflicts in large projects. Each entity needs a different name. 
+   A namespace allows for iddentically named entities as lonog as the namespaces are different.
+
+*/
+
+
+namespace first{
+	int x = 1;
+}
+
+namespace second{
+	int x = 2;
+}
+
+int main(){
+	int x = 3;
+  std::cout << first::x << '\n';   // This prints the value of x found in the "first" namespace.
+  std::cout << second::x << '\n';  // This prints the value of x found in the "second" namespace.
+  std::cout << x << '\n';   // This prints the value of x found in the main function.
+    
+	return 0;
+}
+
+int other(){
+  using namepace first;
+  std::cout << x;
+
+  return 0;
+}
