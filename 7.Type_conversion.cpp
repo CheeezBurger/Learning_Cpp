89e8b01250a3537c1cbc6c53cd7332ed4a709ff2
CheeezBurger <140988965+CheeezBurger@users.noreply.github.com>
2024-08-31 00:31:33 +0000

    Finished type conversion.

--- 7.Type_conversion.cpp
+++ 7.Type_conversion.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
 
+// Type conversion = Conversion of one type of data to another.
+//                   Implicit = Automatic.
+//                   Explicit = Precede valu with new data type (int).
+
+int main(){
+
+    int x = 3.14;
+    std::cout << x << '\n';
+    // This prints 3, the decimal is truncated.
+    // The computer implicitly converts the value from a double to an int.
+
+    double y = (int) 3.14;
+    std::cout << y << '\n';
+    // This prints 3, the decimal is truncated.
+    // The value of y is explicity converted from a double to an int.
+    
+    
+    int correct = 8;
+    int questions = 10;
+    double score = correct/(double)questions * 100;
+    std::cout << score << "%";
+    // This calculates the score as a percentage. 
+    // It is neccessary to explicitly convert the questions int to a double so that it retains the decimal and completes the equation.
+
+    
+    return 0;
+}
