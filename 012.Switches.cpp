78bf8fdae7fc4637e3700522f12e66b6f5fe8705
CheeezBurger <140988965+CheeezBurger@users.noreply.github.com>
2024-08-31 03:28:08 +0000

    Did some more

---
+++ 012.Switches.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+
+
+int month(){
+
+    int month;
+    std::cout << "Enter the month (1-12): ";
+    std::cin >> month;
+
+    switch(month){
+        case 1:     // Cases replace multiple 'else if' statements. They look cleaner and are easier to read.
+            std::cout << "It is January" << '\n';
+            break;
+        case 2:
+            std::cout << "It is February" << '\n';
+            break;
+        case 3:
+            std::cout << "It is March" << '\n';
+            break;
+        case 4:
+            std::cout << "It is April" << '\n';
+            break;
+        case 5:
+            std::cout << "It is May" << '\n';
+            break;
+        case 6:
+            std::cout << "It is June" << '\n';
+            break;
+        case 7:
+            std::cout << "It is July" << '\n';
+            break;
+        case 8:
+            std::cout << "It is August" << '\n';
+            break;
+        case 9:
+            std::cout << "It is September" << '\n';
+            break;
+        case 10:
+            std::cout << "It is October" << '\n';
+            break;
+        case 11:
+            std::cout << "It is November" << '\n';
+            break;
+        case 12:
+            std::cout << "It is December" << '\n';
+            break;
+        default:    // Like an 'else' statement
+            std::cout << "Please enter in only numbers (1-12)" << '\n';
+    }
+
+    return 0;
+}
+
+
+int grade(){
+    
+    char grade;
+
+    std::cout << "What grade did you get? " << '\n';
+    std::cin >> grade;
+
+    switch(grade){
+        case 'A':
+            std::cout << "You did great!"  << '\n';
+            break;
+        case 'B':
+            std::cout << "You did good." << '\n';
+            break;
+        case 'C':
+            std::cout << "You did average." << '\n';
+            break;
+        case 'D':
+            std::cout << "You did bad" << '\n';
+            break;
+        case 'E':
+            std::cout << "You failed miserably." << '\n';
+            break;
+        case 'F':
+            std::cout << "You're a FAILIURE." << '\n';
+            break;
+        default:
+            std::cout << "Please only enter in a letter grade (A-F)." << '\n';
+    }
+
+    return 0;
+}
+
+int x = 3;
+std::cout << x;
