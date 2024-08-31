12ecfe5ea131a4f32530600eed5e743efa75a83a
CheeezBurger <140988965+CheeezBurger@users.noreply.github.com>
2024-08-31 01:36:40 +0000

    Finished other maths functions

--- 9.Other_maths_functions.cpp
+++ 9.Other_maths_functions.cpp
@@ -1,13 +1,37 @@
 #include <iostream>
+#include <cmath>
 
 int main(){
 
     double x = 3;
     double y = 4;
-    double z;
+    double max;
+    double min;
+    double power;
+    double square_root;
+    double absolute_value;
+    double pi = 3.14;
+    double rounded;
+    double round_up;
+    double round_down;
 
-    z = std::max(x, y);
-    std::cout << z;
+    max = std::max(x, y); // This selects the higher value.
+    min = std::min(x, y); // This selects the lower value.
+    power = pow(2, 3); // This raises 2 to the power of 3.
+    square_root = sqrt(9); // This calculates the square root of 9.
+    absolute_value = abs(-4); // This shows how far the number is from 0.
+    rounded = round(pi); // This rounds the number up or down depending on what is closer.
+    round_up = ceil(pi); // This always rounds the number up.
+    round_down = floor(pi); // This always rounds the number down.
+    
+    std::cout << max << '\n';
+    std::cout << min << '\n';
+    std::cout << power << '\n';
+    std::cout << square_root << '\n';
+    std::cout << absolute_value << '\n';
+    std::cout << rounded << '\n';
+    std::cout << round_up << '\n';
+    std::cout << round_down << '\n';
 
     return 0;
 }
