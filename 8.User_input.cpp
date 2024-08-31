2d4af4a4abf04de0d3722b150aec7d7225ffaead
CheeezBurger <140988965+CheeezBurger@users.noreply.github.com>
2024-08-31 01:16:31 +0000

    Finished user input

--- 8.User_input.cpp
+++ 8.User_input.cpp
@@ -3,6 +3,9 @@
 // cout << (<< = insertion operator)
 // cin >> (>> = extraction operator)
 
+// IMPORTANT - When using this code, make sure to go to file, preferences, settings, and search for code runner. Look for 'run in terminal' and switch it on. 
+// This allows you to accept user input.
+
 int main(){
     
     std::string name;
