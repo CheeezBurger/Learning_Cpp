8f48624d94b6614bb9f58a19fa3f4cccbcb273c0
CheeezBurger <140988965+CheeezBurger@users.noreply.github.com>
2024-09-04 12:29:02 +0800

    Update 012.Switches.cpp

--- 012.Switches.cpp
+++ 012.Switches.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 
+// IMPORTANT - When using this code, make sure to go to file, preferences, settings, and search for code runner. Look for 'run in terminal' and switch it on. 
+// This allows you to accept user input.
+
 int main(){
 
     int month;
