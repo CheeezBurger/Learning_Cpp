8e3464bdb78a0a9cd5eb51e65baf74dd7c3fedba
CheeezBurger <140988965+CheeezBurger@users.noreply.github.com>
2024-10-24 11:37:45 +0000

    Fixed an issue

--- 006.Arithmetic_operators.cpp
+++ 006.Arithmetic_operators.cpp
@@ -9,28 +9,35 @@ int main(){
     int class2 = 20;
     int class3 = 20;
     int class4 = 20;
+    int class5 = 20;
 
-    //students = students + 1;  This can be used, but is long
-    //students++;   This adds only one.
-    //students+=2;  This is the ideal one to use.
-    //addition (+), subraction (-), multiplication (*), division (/).
-    //the modulus operator shows the remainder of a number. 
-    // int students = 20
-    // int remainder = students % 3;
-    // std::cout << remainder;
-    // This would produce 1, which is the remainder of 20 / 3.
+    // students = students + 1;  This can be used, but is long
+    // students++;   This adds only one.
+    // students+=2;  This is the ideal one to use.
+
+    // Addition (+), subraction (-), multiplication (*), division (/).
+
+    // The modulus operator '%' shows the remainder of a number after it is divided by another number. 
 
     class1+=10;
     std::cout << class1 << '\n';
+    // 20 + 10
 
     class2-=10;
     std::cout << class2 <<'\n';
+    // 20 - 10
 
     class3*=10;
     std::cout << class3 << '\n';
+    // 20 x 10
 
     class4/=10;
     std::cout << class4 << '\n';
+    // 20 / 10
+
+    int remainder = class5 % 3;
+    std::cout << remainder << '\n';
+    // the remainder of 20 / 3
 
     return 0;
 }
--- 014.Ternary_Operators.cpp
+++ 014.Ternary_Operators.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 
+// Ternary operator '?:' is a replacement for the if/else statement.
+// condition ? expression1 : expression2;
+//               If true      If false
 int main()
 {
-    
+    int grade = 75;
+    grade >= 50 ? std::cout << "You passed!" : std::cout << "You failed...";
+    // If grade is greater than or equal to 50, print "You passed!", if not, print "You failed..."
 
+    
 
     return 0;
 }
