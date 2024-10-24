89cccd37707173e91740e44a1ce6779cdc30e45a
CheeezBurger <140988965+CheeezBurger@users.noreply.github.com>
2024-10-24 11:53:35 +0000

    Finished Ternary Operators

--- 014.Ternary_Operators.cpp
+++ 014.Ternary_Operators.cpp
@@ -9,8 +9,15 @@ int main()
     grade >= 50 ? std::cout << "You passed!" << '\n' : std::cout << "You failed..." << '\n';
     // If grade is greater than or equal to 50, print "You passed!", if not, print "You failed..."
 
-    int number = 9;
-    number % 2 == 1 ? std::cout << "ODD" : std::cout << "Even";
+    int number = 10;
+    number % 2 == 1 ? std::cout << "ODD" << '\n' : std::cout << "Even" << '\n';
+    // If the number has a remainder of 1, it is odd. If not, it is even.
+
+    bool hungry = false;
+    hungry ? std::cout << "You are hungry" << '\n' : std::cout << "You are full" << '\n';
+    // To save space, you could also write it as:
+    std::cout << (hungry ? "You are hungry" : "You are full") << '\n';
+    // This does the same thing, just shorter.
 
     return 0;
 }
